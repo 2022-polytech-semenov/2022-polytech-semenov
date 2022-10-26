95a08677d11651aff25697503d5e32e09d2b9ba5
unknown <urii-sem@mail.ru>
2022-10-26 19:59:13 +0300

    fix: calculator.cpp edited

--- 03_cpp/calculator/calculator.cpp
+++ 03_cpp/calculator/calculator.cpp
@@ -1,49 +1,41 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-    char o;
-    float n1, n2, n12;
-
-    cout << "Enter the operator (+, -, *, /, ^): ";
-    cin >> o;
-
-    cout << "Enter the operands: ";
-    cin >> n1 >> n2;
-
-    n12 = n1;
-
-    switch (o) {
-
-    case '+':
-        cout << n1 << " + " << n2 << " = " << n1 + n2;
-        break;
-
-    case '-':
-        cout << n1 << " - " << n2 << " = " << n1 - n2;
-        break;
-
-    case '*':
-        cout << n1 << " * " << n2 << " = " << n1 * n2;
-        break;
-
-    case '/':
-        cout << n1 << " / " << n2 << " = " << n1 / n2;
-        break;
-
-    case '^':
-        for (int i = 1; i < n2; i++) {
-            n12 = n12 * n1;
-        }
-        cout << n1 << " ^ " << n2 << " = " << n12;
-        break;
-
-    default:
-        cout << "Mistake! The menu item is missing?";
-        break;
-    }
-
-    return 0;
-
+	char o;
+	double a, b;
+	while (true)
+	{
+		cout << "enter your exercice" << endl;
+		cin >> a >> o >> b;
+		if (o == '+')
+		{
+			cout << a + b << endl;
+		}
+		if (o == '-')
+		{
+			cout << a - b << endl;
+		}
+		if (o == '*')
+		{
+			cout << a * b << endl;
+		}
+		if (o == '/')
+		{
+			if (b == 0) {
+				cout << "Error" << endl;
+			}
+			else {
+				cout << a / b << endl;
+			}
+		}
+		if (o == '^')
+		{
+			cout << pow(a, b) << endl;
+		}
+	}
+	return 0;
 }
