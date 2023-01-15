69745be44e92030ac96fa8d499fc335e6b65c892
Yurii Semenov <urii-sem@mail.ru>
2023-01-15 23:54:50 +0300

    fix: updated

--- 10_list/reverse/test_reverse_ll.cpp
+++ 10_list/reverse/test_reverse_ll.cpp
@@ -1,22 +1,31 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "reverse_ll.hpp"
+#include "Reverse.h"
 #include <deque>
 #include <doctest.h>
 
-std::deque<int> ll_to_deque(Node** head) {
+
+using namespace std;
+
+
+deque<int> ll_to_deque(Node** head)
+{
     auto tmp = *head;
-    std::deque<int> out{};
-    while (tmp != nullptr) {
+    deque<int> out{};
+    while (tmp != nullptr)
+    {
         out.push_front(tmp->data);
         tmp = tmp->next;
     }
+
     return out;
 }
 
-TEST_CASE("aa") {
-    std::deque<int> v1{ 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-    std::deque<int> v2{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    std::deque<int> v3{ 1, 2, 6, 5, 4, 3, 7, 8, 9 };
+
+TEST_CASE("Reverse")
+{
+    deque<int> v1{ 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    deque<int> v2{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    deque<int> v3{ 1, 7, 6, 5, 4, 3, 2, 8, 9 };
 
     Node* head = generate(9);
     CHECK(v1 == ll_to_deque(&head));
@@ -24,6 +33,6 @@ TEST_CASE("aa") {
     reverse(&head);
     CHECK(v2 == ll_to_deque(&head));
 
-    reverse(&head, 3, 6);
+    reverse(&head, 2, 7);
     CHECK(v3 == ll_to_deque(&head));
 }
